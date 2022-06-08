f6e2250855c42b741e43008fc0c8768281f35a1f
Jae Cho <jjaecho@gmail.com>
2022-06-07 22:02:30 -0400

    Initial Commit

---
+++ C6_FriendFace/C6_FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  C6_FriendFaceApp.swift
+//  C6_FriendFace
+//
+//  Created by Jae Cho on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct C6_FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
